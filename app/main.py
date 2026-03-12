0d0063c369c2eb05bbb330bcd4f5c59833c0cb5f
gglolppc <gglolppc@gmail.com>
2026-03-12 12:36:15 +0200

    mddl

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ from app.api.routes.auth import router as auth_router
 from app.web.routes.pages import router as pages_router
 from app.web.routes.admin import router as admin_pages_router
 from app.web.routes.admin_auth import router as admin_login
-
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 app = FastAPI(title="Psychologist Site")
 
-
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
